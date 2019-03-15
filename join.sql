ec83dea9ec75cfda9201735449a12b80c4215143
niavarani_a@hotmail.com <niavarani_a@hotmail.com>
2019-03-15 09:49:49 -0700

    initial join sql

---
+++ join.sql
@@ -0,0 +1,4 @@
+/*
+Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+*/
+
